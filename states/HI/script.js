097f3198726e29fbbf5affa9e523f87480866842
Jason C <Jason C>
2019-03-07 23:27:10 -0500

    Add HI Gov JS for region

--- states/HI/script.js
+++ states/HI/script.js
@@ -1,69 +1,52 @@
-
-const zipList = [96719, 96755, 96743, 96738, 96740, 96725, 96750, 96704, 96722, 96726, 96763];
-
-const countyHash = {
-  "Hawaii" : "East Hawai'i Island",
-  "Honululu" : "O'ahu",
-  "Kalawao" : "Molaka'i",
-  "Kauai" : "Kaua'i",
-  "Maui" : "Maui" // except for 96763 which is Lana'i
-}
-
-function findRegion(county,zip){
-  console.log(`Running... for ${ county }, ${ zip }`);
-  // Check the zip zipList
-  //  if the zip is in the zip list and is 96763, then the region is "Lana'i"
-  if (zip == 96763) {
-    console.log("lanai");
-    return "Lana'i";
-  }
-  //  if the zip is in the zip list and not 96763, the region is "West Hawai'i Island"
-  else if (zipList.includes(zip)) {
-    console.log("west HI");
-    return "West Hawai'i Island";
-  }
-  //  if the zip is not in the zip list, retrieve the value from the countyHash,
-  else {
-    console.log(`${ countyHash[county] }`);
-    return countyHash[county];
-  }
-}
-
-// s/b lanai
-findRegion("some county", 96763);
-
-// s/b west Hawaii
-findRegion("some county", 96755);
-
-// s/b east Hawaii
-findRegion("Hawaii", 12345);
-
-
-
-
-// County : Dropdown
-// Hawaii : East Hawai'i Island except for zip codes below*
-// Honululu : O'ahu
-// Kalawao : Molaka'i
-// Kauai : Kaua'i
-// Maui : Maui except for 96763 which is Lana'i
 //
-// West Hawai'i Island:
-// 96719, 96755, 96743, 96738, 96740, 96725, 96750, 96704, 96722, 96726
+// ** Special handling for Hawaii governor. **
+// Hawaii has a 'region' dropdown that doesn't correlate directly to any address component we currently collect.  So we have connected the region to a county where we can, and a zip code where we cannot.  See comments in this file for more details.
 
+// This JS is included in a separate file for readability and ease of editing.  All comments have been removed, and copied into the yaml.  The same steps should take place should this JS need to be edited.
+//
 
-// Minimized:
+// The form config has an address line 2 field.  A step in the form_config will put the $ADDRESS_COUNTY value in it so we can use it in this script, then later clear it so it is not actually submitted with the form.
+var county = document.getElementById('input_1_9_2').value;
+var zip = document.getElementById('input_1_9_5').value;
 
-const zipList=[96719,96755,96743,96738,96740,96725,96750,96704,96722,96726,96763];const countyHash={"Hawaii":"East Hawai'i Island","Honululu":"O'ahu","Kalawao":"Molaka'i","Kauai":"Kaua'i","Maui":"Maui"}
-function findRegion(county,zip){if(zip==96763){return"Lana'i"}
-else if(zipList.includes(zip)){return"West Hawai'i Island"}
-else{return countyHash[county]}}
+// These are the zips where we determine the region by the zip code rather than county.
+const zipList = ['96719', '96755', '96743', '96738', '96740', '96725', '96750', '96704', '96722', '96726', '96763']
 
+// These are the regions we can determine by county.  NOTE: The apostrophes in the values of this hash (the region names) are right apostrophes (Unicode U+2019).  The regular straight apostrophe (Unicode U+0027) will not match on the form.
+const countyHash = {
+  'Hawaii' : 'East Hawaiʻi Island',
+  'Honolulu' : 'Oʻahu',
+  'Kalawao' : 'Molokaʻi',
+  'Kauai' : 'Kauaʻi',
+  'Maui' : 'Maui' // except for 96763 which is Lanaʻi
+}
+
+var region = function findRegion(county,zip) {
+  // if the zip is in the zip list and is 96763, then the region is 'Lanaʻi
+  if (zip == '96763') {
+    var r = 'Lanaʻi';
+  }
+  // if the zip is in the zip list and not 96763, the region is 'West Hawaiʻi Island'
+  else if (zipList.includes(zip)) {
+    var r = 'West Hawaiʻi Island';
+  }
+  // if the zip is not in the zip list, retrieve the value from the countyHash
+  else {
+    var r = countyHash[county];
+  }
+  return r;
+}
+
+document.getElementById('input_1_18').value = region(county,zip);
+// Clear out the address 2 field:
+document.getElementById('input_1_9_2').value ='';
+
+// Some tests
 // s/b lanai
-findRegion("some county", 96763);
+// findRegion('some county', 96763);
 
 // s/b west Hawaii
-findRegion("some county", 96755);
+// findRegion('some county', 96755);
 
 // s/b east Hawaii
-findRegion("Hawaii", 12345);
+// findRegion('Hawaii', 12345);
